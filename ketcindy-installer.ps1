32afcc4a61c8a842f9109da929460a1f440e90ba
nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-10-10 21:42:55 +0900

    [add] ps1 upload

---
+++ ketcindy-installer.ps1
@@ -0,0 +1,49 @@
+mkdir $env:TEMP/AUTOKETCINDYINSTALL
+cd $env:TEMP/AUTOKETCINDYINSTALL
+
+$path_Cinderella = "https://beta.cinderella.de/Cinderella-3.0b.2080-64bit.exe"
+$path_kettex = "https://github.com/ketpic/kettex/releases/download/v0.20230320/KeTTeX-windows-20230320.zip"
+$path_R = "https://cran.r-project.org/bin/windows/base/R-4.3.1-win.exe"
+$path_sumatra = "https://www.sumatrapdfreader.org/dl/rel/3.4.6/SumatraPDF-3.4.6-64-install.exe"
+$path_maxima = "https://jaist.dl.sourceforge.net/project/maxima/Maxima-Windows/5.47.0-Windows/maxima-5.47.0-win64.exe"
+$path_ketcindy = "https://github.com/ketpic/ketcindy/archive/refs/tags/4.4.44.zip"
+
+echo "Downloading Cinderella..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_Cinderella -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/Cinderella-3.0b.2080-64bit.exe"
+echo "Installing Cinderella..."
+.\Cinderella-3.0b.2080-64bit.exe -q
+
+echo "Downloading KeTTeX..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_kettex -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/kettex.zip"
+echo "Unzipping KeTTeX..."
+Expand-Archive .\kettex.zip c:\kettex-installer
+echo "Installing KeTTeX..."
+Start-Process -FilePath "C:\kettex-installer\kettexinst.cmd"
+
+echo "Downloading R..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_R -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/R-4.3.1-win.exe"
+echo "Installing R..."
+.\R-4.3.1-win.exe /silent
+
+echo "Downloading SumatraPDF..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_sumatra -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/SumatraPDF-3.4.6-64-install.exe"
+echo "Installing SumatraPDF..."
+.\SumatraPDF-3.4.6-64-install.exe -s -d "C:\Program Files\SumatraPDF"
+
+echo "Downloading Maxima..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_maxima -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/maxima-5.47.0-win64.exe"
+echo "Installing Maxima..."
+.\maxima-5.47.0-win64.exe /S
+
+echo "Downloading KeTCindy..."
+Invoke-WebRequest -UseBasicParsing -Uri $path_ketcindy -OutFile "$env:TEMP/AUTOKETCINDYINSTALL/ketcindydl.zip"
+
+echo "Unzipping KeTCindy..."
+Expand-Archive ./ketcindydl.zip c:/ketcindydl
+
+echo "Installing KeTCindy..."
+ren C:\ketcindydl\ketcindy-4.4.44 ketcindy4.4.44
+
+echo "Opening ketcindysettings.cdy..."
+start C:\ketcindydl\ketcindy4.4.44\doc\ketcindysettings.cdy
+
